4be8ad73e1f22cd4f244b7993174f142283ae751
Entony Siminhuk. <entonycco@gmail.com>
2023-09-28 21:41:54 -0300

    codigo

---
+++ src/Jogador.java
@@ -0,0 +1,19 @@
+public class Jogador {
+    private String nome;
+    private String TipoJogador;
+    private JogoGeneral jogoG;
+
+
+    public void jogarDados(){
+
+    }
+
+    public int escolherJogada(){
+
+    }
+
+    public void mostrarJogadas(){
+
+    }
+
+}
